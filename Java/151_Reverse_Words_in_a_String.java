8414f660d62aff866a7f9dd23085646a985126c6
wun-yu-lin <76545619+wun-yu-lin@users.noreply.github.com>
2024-12-29 14:20:23 +0800

    151_Reverse_Words_in_a_String

---
+++ Java/151_Reverse_Words_in_a_String.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String reverseWords(String s) {
+        String[] strs = s.split(" ");
+        int index = 0;
+        StringBuilder sb = new StringBuilder();
+        for (int i = strs.length -1; i >=0; i--) {
+            String currStr = strs[i];
+            //emptyString 跳過
+            if (currStr == null || currStr.isEmpty()) {
+                continue;
+            }
+            //開頭
+            if (index != 0) {
+                sb.append(" ");
+            }
+            index++;
+            sb.append(currStr);
+        }
+        return sb.toString();
+    }
+}
